eaf0ce9ccfe885040665d262d86d3a1960cd1b6f
Hassan Zoghly <hassanzogly@gmail.com>
2026-02-20 21:18:41 +0200

    Process files

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,11 +1,13 @@
+from importlib.resources import contents
 from fastapi import APIRouter, FastAPI, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 import os
 from helpers.config import get_settings, Settings
-from controllers import DataController, ProjectController
+from controllers import DataController, ProjectController, ProcessController
 from models import ResponseSignal
 import aiofiles
 import logging
+from .schemes.data import ProcessRequest
 
 logger = logging.getLogger("uvicorn.error") # to show the problems of something happend
 
@@ -49,3 +51,31 @@ async def upload_data(project_id: str, file: UploadFile,
         "Signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
         "File_id": file_id
     })
+
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+
+    process_controller = ProcessController(project_id=project_id)
+
+    file_content = process_controller.get_file_content(file_id=file_id)
+
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    )
+
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            content={
+                "Signal": ResponseSignal.PROCESSING_FAILED
+            }
+        )
+
+    return file_chunks
